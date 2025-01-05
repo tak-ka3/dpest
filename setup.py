ca2ba7369bbd82d8b241fbff94be57cea5c988d6
TakumiHiraoka <hi.ta9.23.333@gmail.com>
2025-01-05 11:57:01 +0900

    fix: setting for library dpest

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dpest",                  # パッケージ名
+    version="0.1.0",                    # バージョン
+    description="dpest",  # 説明文
+    author="tak-ka3",                 # 作者名
+    url="https://github.com/yourusername/my_library",  # プロジェクトのURL
+    packages=find_packages(),           # パッケージを自動検出
+    install_requires=[                  # 依存パッケージ
+        # 必要に応じて依存ライブラリを記載
+        "numpy",
+        "matplotlib",
+        "scipy",
+        "mmh3"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires=">=3.6",            # 対応するPythonバージョン
+)
