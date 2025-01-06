e1fa88f0c1290231707888dc93f82b9184c3d9b1
tak-ka3 <hi.ta9.23.333@gmail.com>
2025-01-06 14:04:57 +0900

    fix: enable to specify input size

--- test/alg/noisy_arg_max_lap.py
+++ test/alg/noisy_arg_max_lap.py
@@ -3,14 +3,14 @@ from dpest.input import InputArray
 from dpest.utils import laplace_extract
 from dpest.func import eps_est
 
+INPUT_ARR_SIZE = 5
 eps = 0.1
 sens = 1
-Lap1, Lap2, Lap3, Lap4, Lap5 = laplace_extract(InputArray(5, adj="inf"), sens/(eps/2))
-LapArr = [Lap1, Lap2, Lap3, Lap4, Lap5]
+LapArr = list(laplace_extract(InputArray(INPUT_ARR_SIZE, adj="inf"), sens/(eps/2)))
 
 max_val = LapArr[0]
 max_ind = 0
-for i in range(5):
+for i in range(INPUT_ARR_SIZE):
     max_ind = Br(max_val, LapArr[i], max_ind, i)
     max_val = Max(max_val, LapArr[i])
 Y = max_ind
--- test/alg/noisy_max_lap.py
+++ test/alg/noisy_max_lap.py
@@ -3,9 +3,12 @@ from dpest.input import InputArray
 from dpest.utils import laplace_extract
 from dpest.func import eps_est
 
+INPUT_ARR_SIZE = 5
 eps = 0.1
 sens = 1
-Lap1, Lap2, Lap3, Lap4, Lap5 = laplace_extract(InputArray(5, adj="inf"), sens/eps)
+LapArr = list(laplace_extract(InputArray(INPUT_ARR_SIZE, adj="inf"), sens/eps))
 
-Y = Max(Max(Max(Max(Lap1, Lap2), Lap3), Lap4), Lap5)
+Y = LapArr[0]
+for i in range(1, INPUT_ARR_SIZE):
+    Y = Max(Y, LapArr[i])
 eps = eps_est(Y)
